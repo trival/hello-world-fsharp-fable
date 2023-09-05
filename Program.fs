740b7d07bc246c7af8d66ac96c749c37bacc66c6
Thomas Gorny <thomas.gorny@pm.me>
2023-09-05 22:48:13 +0200

    start ray tracer

--- Program.fs
+++ Program.fs
@@ -1,4 +1,7 @@
-﻿open Browser
+﻿module Program
+
+open Browser
+open Vectors.Vec3F64
 
 printfn "Hello from F#"
 
@@ -8,4 +11,10 @@ document.body.appendChild div |> ignore
 
 let add x y = x + y
 
+let color = vec3 1. 0. 0.
+let color2 = vec3 0. 1. 1.
+let cross = color.cross color2
+
+
 printf "1 + 2 = %f" (add 1.0 2.0)
+printf "%s %f" (cross.ToString()) (cross.length ())
---
+++ src/Vectors.fs.js
@@ -0,0 +1,21 @@
+import { class_type } from "../fable_modules/fable-library.4.1.4/Reflection.js";
+
+export class Vec3 {
+    constructor(x, y, z) {
+        this.vec = (new Float64Array([x, y, z]));
+    }
+    toString() {
+        let copyOfStruct, copyOfStruct_1, copyOfStruct_2;
+        const a = this;
+        return ((((("Vec3(" + ((copyOfStruct = a.vec[0], copyOfStruct.toString()))) + ",") + ((copyOfStruct_1 = a.vec[1], copyOfStruct_1.toString()))) + ",") + ((copyOfStruct_2 = a.vec[2], copyOfStruct_2.toString()))) + ")";
+    }
+}
+
+export function Vec3_$reflection() {
+    return class_type("Vectors.Vec3F64.Vec3", void 0, Vec3);
+}
+
+export function Vec3_$ctor_Z7AD9E565(x, y, z) {
+    return new Vec3(x, y, z);
+}
+
